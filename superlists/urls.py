bfd6ac2ed4879e87c80c7e4c12322bac27b083f5
ralicagulubova <rgylybova@melontech.com>
2014-05-29 18:21:55 +0300

    new URL

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'lists.views.home_page', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+
+urlpatterns = patterns('',
+    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+    	name ='view_list'
+    ),
     #url(r'^admin/', include(admin.site.urls)),
 )
